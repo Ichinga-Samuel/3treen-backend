b95629347f6771cf364e9949e307cd3cc825a2a5
Victor Maduforo <victormaduforo@gmail.com>
2021-07-20 14:53:54 +0100

    resolved conflict

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,22 +82,6 @@ exports.getMe = catchAsync(async (req, res, next) => {
 exports.getAllUsers = factory.getAll(User);
 
 // this fetch all users without pagination
-<<<<<<< HEAD
-exports.getAllRawUsers = catchAsync(async (req, res, next) => {
-  const allUsers = await User.find({});
-  if (allUsers.length > 0) {
-    res.status(200).json({
-      status: 'success',
-      length: allUsers.length,
-      All_Users: allUsers,
-    });
-  } else {
-    res.status(400).json({
-      status: 'not fount or somthing went wrong',
-    });
-  }
-});
-=======
 exports .getAllRawUsers = catchAsync(async (req,res,next)=>{
   const allUsers = await User.find({})
   if(allUsers.length > 0){
@@ -112,7 +96,6 @@ exports .getAllRawUsers = catchAsync(async (req,res,next)=>{
     })
   }
 })
->>>>>>> refs/remotes/origin/feature-customer-service
 
 exports.updateUserRole = catchAsync(async (req, res, next) => {
   const { userId, role } = req.params;
